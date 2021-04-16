b7e36a0b92c7b764eb8b71dabb39d5323e3a2b4c
Raynaldi262 <ginantaraaldi1387@gmail.com>
2021-04-17 00:41:03 +0700

    gbr

--- Eshopper/product_details.php
+++ Eshopper/product_details.php
@@ -1,18 +1,18 @@
 <?php require('../connect/conn.php');
 require('../model/User.php');
-if(isset($_SESSION['cust_id'])){
+if (isset($_SESSION['cust_id'])) {
 	$datauser = getDataUser($_SESSION['cust_id']);
 	$data_cart = getcartCount($_SESSION['cust_id']);
 	$data_check = getcheckCount($_SESSION['cust_id']);
 	$proses_count = getProsesCount($_SESSION['cust_id']);
-}else{
+} else {
 	$data_cart['juml'] = 0;
 	$data_check['juml'] = 0;
 	$proses_count['juml'] = 0;
 }
 
- if(isset($_GET['id'])){
- 	$id_item = $_GET['id'];
+if (isset($_GET['id'])) {
+	$id_item = $_GET['id'];
 	$data = getDetailitem($id_item);
 	$data_type = getTypeitem($data['type_id']);
 	$data_color = getColoritem($data['color_id']);
@@ -22,33 +22,37 @@ if(isset($_SESSION['cust_id'])){
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <title>Product Details | E-Shopper</title>
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/font-awesome.min.css" rel="stylesheet">
-    <link href="../css/prettyPhoto.css" rel="stylesheet">
-    <link href="../css/price-range.css" rel="stylesheet">
-    <link href="../css/animate.css" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>Product Details | E-Shopper</title>
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+	<link href="../css/font-awesome.min.css" rel="stylesheet">
+	<link href="../css/prettyPhoto.css" rel="stylesheet">
+	<link href="../css/price-range.css" rel="stylesheet">
+	<link href="../css/animate.css" rel="stylesheet">
 	<link href="../css/main.css" rel="stylesheet">
 	<link href="../css/responsive.css" rel="stylesheet">
-    <!--[if lt IE 9]>
+	<!--[if lt IE 9]>
     <script src="js/html5shiv.js"></script>
     <script src="js/respond.min.js"></script>
-    <![endif]-->       
-    <link rel="shortcut icon" href="images/ico/favicon.ico">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../images/ico/apple-touch-icon-114-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../images/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../images/ico/apple-touch-icon-72-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" href="../images/ico/apple-touch-icon-57-precomposed.png">
-</head><!--/head-->
+    <![endif]-->
+	<link rel="shortcut icon" href="images/ico/favicon.ico">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="../images/ico/apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="../images/ico/apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="../images/ico/apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon-precomposed" href="../images/ico/apple-touch-icon-57-precomposed.png">
+</head>
+<!--/head-->
 
 <body>
-	<header id="header"><!--header-->		
-		<div class="header-middle"><!--header-middle-->
+	<header id="header">
+		<!--header-->
+		<div class="header-middle">
+			<!--header-middle-->
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-4">
@@ -60,54 +64,52 @@ if(isset($_SESSION['cust_id'])){
 						<div class="shop-menu pull-right">
 							<ul class="nav navbar-nav">
 								<?php
-								if(!isset($_SESSION['cust_id'])){
-									?>
+								if (!isset($_SESSION['cust_id'])) {
+								?>
 									<li><a href="../logout.php"><i class="fa fa-user"></i> Account</a></li>
-									<?php
-								}else{
+								<?php
+								} else {
 									$data_user = custLogin($_SESSION['cust_id']);
-									?>
-								<?php if($proses_count['juml'] != 0){?>
-								<li><a href="../profile.php"><img src="../images/Profile/<?php echo $datauser['cust_img']?>" 
-									 style=" width:25px;height: 25px; border-radius: 50%;"/>
-									 <span><?php echo $datauser['cust_name'];?></span>
-								 	<span class="badge"><?php echo $proses_count['juml']; ?></span>
-								 	</a>
-								</li>
-								<?php }else{ ?>
-									<li><a href="../profile.php"><img src="../images/Profile/<?php echo $datauser['cust_img']?>" 
-								 style=" width:25px;height: 25px; border-radius: 50%;"/> <?php echo $datauser['cust_name'];?></a></li>
-								<?php } ?>
+								?>
+									<?php if ($proses_count['juml'] != 0) { ?>
+										<li><a href="../profile.php"><img src="../images/Profile/<?php echo $datauser['cust_img'] ?>" style=" width:25px;height: 25px; border-radius: 50%;" />
+												<span><?php echo $datauser['cust_name']; ?></span>
+												<span class="badge"><?php echo $proses_count['juml']; ?></span>
+											</a>
+										</li>
+									<?php } else { ?>
+										<li><a href="../profile.php"><img src="../images/Profile/<?php echo $datauser['cust_img'] ?>" style=" width:25px;height: 25px; border-radius: 50%;" /> <?php echo $datauser['cust_name']; ?></a></li>
+									<?php } ?>
 								<?php
 								}
 								?>
 								<!-- checkout -->
-								<?php if($data_check['juml'] != 0){?>
-								<li><a href="../checkout.php" class="notification"><i class="fa fa-shopping-cart"></i>												
-									<span>Checkout</span>
- 									<span class="badge"><?php echo $data_check['juml']; ?></span>
-									</a>
-								</li>
-								<?php }else{ ?>
+								<?php if ($data_check['juml'] != 0) { ?>
+									<li><a href="../checkout.php" class="notification"><i class="fa fa-shopping-cart"></i>
+											<span>Checkout</span>
+											<span class="badge"><?php echo $data_check['juml']; ?></span>
+										</a>
+									</li>
+								<?php } else { ?>
 									<li><a href="../checkout.php" class="notification"><i class="fa fa-crosshairs"></i>Checkout</a></li>
 								<?php } ?>
 								<!-- cart -->
-								<?php if($data_cart['juml'] != 0){?>
-								<li><a href="../cart.php" class="notification"><i class="fa fa-shopping-cart"></i>												
-									<span>Cart</span>
- 									<span class="badge"><?php echo $data_cart['juml']; ?></span>
-									</a>
-								</li>
-								<?php }else{ ?>
+								<?php if ($data_cart['juml'] != 0) { ?>
+									<li><a href="../cart.php" class="notification"><i class="fa fa-shopping-cart"></i>
+											<span>Cart</span>
+											<span class="badge"><?php echo $data_cart['juml']; ?></span>
+										</a>
+									</li>
+								<?php } else { ?>
 									<li><a href="../cart.php" class="notification"><i class="fa fa-shopping-cart"></i>Cart</a></li>
 								<?php } ?>
 								<?php
-								if(!isset($_SESSION['cust_id'])){
-									?>
+								if (!isset($_SESSION['cust_id'])) {
+								?>
 									<li><a href="../login.php"><i class="fa fa-lock"></i> Login</a></li>
-									<?php
-								}else{
-									?>
+								<?php
+								} else {
+								?>
 									<li><a href="../login_user/logout_user.php"><i class="fa fa-lock"></i> Logout</a></li>
 								<?php
 								}
@@ -117,9 +119,11 @@ if(isset($_SESSION['cust_id'])){
 					</div>
 				</div>
 			</div>
-		</div><!--/header-middle-->
-	
-		<div class="header-bottom"><!--header-bottom-->
+		</div>
+		<!--/header-middle-->
+
+		<div class="header-bottom">
+			<!--header-bottom-->
 			<div class="container">
 				<div class="row">
 					<div class="col-sm-9">
@@ -140,43 +144,50 @@ if(isset($_SESSION['cust_id'])){
 					</div>
 				</div>
 			</div>
-		</div><!--/header-bottom-->
-	</header><!--/header-->
-	
+		</div>
+		<!--/header-bottom-->
+	</header>
+	<!--/header-->
+
 	<section>
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-11">
-					<div class="product-details"><!--product-details-->
+					<div class="product-details">
+						<!--product-details-->
 						<div class="col-sm-5">
 							<div class="view-product">
-								<img style="width:500px" src="dist/img/item/<?php echo $data['item_img'];?>" alt="" />
+								<img style="width:500px" src="../../dist/img/item/<?php echo $data['item_img']; ?>" alt="" />
 							</div>
 						</div>
 						<div class="col-sm-7">
-							<div class="product-information" style="float:right"><!--/product-information-->
-								<h2><?php echo $data['item_name'];?></h2>
+							<div class="product-information" style="float:right">
+								<!--/product-information-->
+								<h2><?php echo $data['item_name']; ?></h2>
 								<span>
 									<span>Rp. <?php echo number_format($data['item_price']); ?></span>
 									<label>Quantity:</label>
-										<form action="./model/User.php" method="post">
-											<input type="number" min='1' value="1" required name="qty"/>
-											<input type="hidden" name="item_id" value="<?php echo $data['item_id']?>">
-											<button type="submit" name="addchart" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
-										</form>	
+									<form action="./model/User.php" method="post">
+										<input type="number" min='1' value="1" required name="qty" />
+										<input type="hidden" name="item_id" value="<?php echo $data['item_id'] ?>">
+										<button type="submit" name="addchart" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</button>
+									</form>
 								</span>
-								<p><b>Warna: </b><?php echo $data_color['color_name'];?></p>
-								<p><b>Size: </b> <?php echo $data['item_size'];?></p>
-								<p><b>Kategori: </b> <?php echo $data_type['type_name'];?></p>
-								<p><b>Deskripsi: </b> <?php echo $data['item_desc'];?></p>
-							</div><!--/product-information-->
+								<p><b>Warna: </b><?php echo $data_color['color_name']; ?></p>
+								<p><b>Size: </b> <?php echo $data['item_size']; ?></p>
+								<p><b>Kategori: </b> <?php echo $data_type['type_name']; ?></p>
+								<p><b>Deskripsi: </b> <?php echo $data['item_desc']; ?></p>
+							</div>
+							<!--/product-information-->
 						</div>
-					</div><!--/product-details-->
+					</div>
+					<!--/product-details-->
 				</div>
 			</div>
-		</section>
-	
-	<footer id="footer"><!--Footer-->
+	</section>
+
+	<footer id="footer">
+		<!--Footer-->
 		<div class="footer-top">
 			<div class="container">
 				<div class="row">
@@ -201,7 +212,7 @@ if(isset($_SESSION['cust_id'])){
 								<h2>24 DEC 2014</h2>
 							</div>
 						</div>
-						
+
 						<div class="col-sm-3">
 							<div class="video-gallery text-center">
 								<a href="#">
@@ -216,7 +227,7 @@ if(isset($_SESSION['cust_id'])){
 								<h2>24 DEC 2014</h2>
 							</div>
 						</div>
-						
+
 						<div class="col-sm-3">
 							<div class="video-gallery text-center">
 								<a href="#">
@@ -231,7 +242,7 @@ if(isset($_SESSION['cust_id'])){
 								<h2>24 DEC 2014</h2>
 							</div>
 						</div>
-						
+
 						<div class="col-sm-3">
 							<div class="video-gallery text-center">
 								<a href="#">
@@ -256,15 +267,17 @@ if(isset($_SESSION['cust_id'])){
 				</div>
 			</div>
 		</div>
-	</footer><!--/Footer-->
-	
+	</footer>
+	<!--/Footer-->
 
-  
-    <script src="../js/jquery.js"></script>
+
+
+	<script src="../js/jquery.js"></script>
 	<script src="../js/price-range.js"></script>
-    <script src="../js/jquery.scrollUp.min.js"></script>
+	<script src="../js/jquery.scrollUp.min.js"></script>
 	<script src="../js/bootstrap.min.js"></script>
-    <script src="../js/jquery.prettyPhoto.js"></script>
-    <script src="../js/main.js"></script>
+	<script src="../js/jquery.prettyPhoto.js"></script>
+	<script src="../js/main.js"></script>
 </body>
+
 </html>
